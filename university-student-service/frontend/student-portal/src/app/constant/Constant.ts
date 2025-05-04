b786e48f46c18e6a2a599e9fead74a4055a8fc1a
Zar Li Pyone <zarli2008@gmail.com>
2025-05-05 00:56:43 +1200

    Start tracking file/folder again

--- university-student-service/frontend/student-portal/src/app/constant/Constant.ts
+++ university-student-service/frontend/student-portal/src/app/constant/Constant.ts
@@ -1,1 +1,8 @@
 
+export const COURSE_LIST_TABLE_HEADERS = [
+    'Code',
+    'Description',
+    'Points'
+  ];
+
+  export const COURSE_API_URL = 'https://localhost:44313/api/Course';
--- university-student-service/frontend/student-portal/src/main.ts
+++ university-student-service/frontend/student-portal/src/main.ts
@@ -4,9 +4,5 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
-// bootstrapApplication(AppComponent, appConfig)
-//   .catch((err) => console.error(err));
-
-  bootstrapApplication(AppComponent, {
-    providers: [provideRouter(routes)]
-  }).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
